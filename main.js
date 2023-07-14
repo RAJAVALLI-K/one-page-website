b62302ec6e5fe8873dab689db1a007e5bb04a010
RAJAVALLI-K <rajavalli1932bca@gmail.com>
2023-07-14 20:58:35 +0530

    add html css using one page website

---
+++ main.js
@@ -0,0 +1,15 @@
+var owl = $('.brand');
+owl.owlCarousel({
+    items:3,
+    loop:true,
+    margin:10,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true
+});
+$('.play').on('click',function(){
+    owl.trigger('play.owl.autoplay',[2000])
+})
+$('.stop').on('click',function(){
+    owl.trigger('stop.owl.autoplay')
+})
